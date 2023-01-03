fb952d18e2a7828eb785a13ff214b682f947f08b
Chris Brown <chrisb@AltEnergy.local>
2023-01-03 14:58:10 -0500

    First commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,34 @@
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import { getContent, RenderContent, getBuilderSearchParams } from "@builder.io/sdk-qwik";
+
+export const BUILDER_PUBLIC_API_KEY = ('67632afd79db42f7822f7df687144e9a'); // <-- Add your Public API KEY here
+export const BUILDER_MODEL = "aj";
+
+export default component$(() => {
+  const location = useLocation();
+  const builderContentRsrc = useResource$<any>(() => {
+    return getContent({
+      model: BUILDER_MODEL,
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      options: getBuilderSearchParams(location.query),
+      userAttributes: {
+        urlPath: location.pathname || "/",
+      },
+    });
+  });
+
+  return (
+    <Resource
+      value={builderContentRsrc}
+      onPending={() => <div>Loading...</div>}
+      onResolved={(content) => (
+        <RenderContent
+          model={BUILDER_MODEL}
+          content={content}
+          apiKey={BUILDER_PUBLIC_API_KEY}
+        />
+      )}
+    />
+  );
+});
