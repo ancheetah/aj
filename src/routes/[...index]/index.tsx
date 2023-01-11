f6c61ae9bc285d7d23e4c5315e0a3e3a429825f1
AJ <ancheetah@users.noreply.github.com>
2023-01-11 13:46:17 -0500

    add catch-all routing; fix api key

--- src/routes/[...index]/index.tsx
+++ src/routes/[...index]/index.tsx
@@ -2,21 +2,22 @@ import { component$, Resource, useResource$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
 import { getContent, RenderContent, getBuilderSearchParams } from "@builder.io/sdk-qwik";
 
-export const BUILDER_PUBLIC_API_KEY = ('67632afd79db42f7822f7df687144e9a'); // <-- Add your Public API KEY here
+export const BUILDER_PUBLIC_API_KEY = "67632afd79db42f7822f7df687144e9a"; // <-- Add your Public API KEY here
 export const BUILDER_MODEL = "aj";
 
 export default component$(() => {
-  const location = useLocation();
+  const {pathname, query} = useLocation();
   const builderContentRsrc = useResource$<any>(() => {
     return getContent({
       model: BUILDER_MODEL,
       apiKey: BUILDER_PUBLIC_API_KEY,
-      options: getBuilderSearchParams(location.query),
+      options: getBuilderSearchParams(query),
       userAttributes: {
-        urlPath: location.pathname || "/",
+        urlPath: pathname || "/",
       },
     });
   });
+  console.log("Content: ", builderContentRsrc)
 
   return (
     <Resource
